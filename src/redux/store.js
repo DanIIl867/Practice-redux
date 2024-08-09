47e6ded0f0d7519c4a13c7b42d4a1e0b81266799
Daniil <negativdaniil4@gmail.com>
2024-08-09 22:27:33 +0300

    Practice-redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,15 @@
-import { legacy_createStore as createStore} from 'redux'
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import {configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { taskReducer } from './taskSlice';
+import { filtersReducer } from './filterSlice';
 
-// export const initialCouter = (value) => {
-//     return{
-//         type: 'counter/increment',
-//         payload: value
-//     }
-// }
 
-// const rootReduser = (state = initialState, action) => {
-//     switch (action.type) {
-//         case 'counter/increment':
-//             return{
-//                 ...state,
-//                 value: state.value += action.payload
-//             }
-//         default:
-//             return state
-//     }
-// }
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: {value: 5},
-    reducers: {
-        increment(state, action){
-            return {
-                ...state, 
-                value: state.value + action.payload
-            }
-        }
-    }
+export const store = configureStore({
+  reducer: {
+    tasks: taskReducer,
+    filters: filtersReducer
+  },
+ 
 })
 
 
 
-export const store = configureStore({reduser: counterSlice.reducer})
